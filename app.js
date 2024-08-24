75f79264840cbc590832c35c543f32c0ef9727c1
mdsaniulbasirsaz <saniulsaj@gmail.com>
2024-08-24 22:11:40 +0600

    Update

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));  
 });
 app.get('/Education', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));  
+    res.sendFile(path.join(__dirname, 'public', 'education.html'));  
 });
 app.get('/project', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));  
+    res.sendFile(path.join(__dirname, 'public', 'project.html'));  
 });
 
 app.listen(PORT, () => {
