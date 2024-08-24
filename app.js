1d54ce4a9d372c6b9c84e90349c8f6be91550c10
mdsaniulbasirsaz <saniulsaj@gmail.com>
2024-08-24 18:43:51 +0600

    add

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path'); 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'styles')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));  
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
